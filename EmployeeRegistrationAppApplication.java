b1e5c61a06eda326487bd3d2ca07f415f77cb1e6
wilfex81 <84175893+wilfex81@users.noreply.github.com>
2021-10-19 08:21:44 +0300

    Add files via upload

---
+++ EmployeeRegistrationAppApplication.java
@@ -0,0 +1,16 @@
+package com.codewithflex;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class EmployeeRegistrationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRegistrationAppApplication.class, args);
+	}
+
+}
---
+++ EmployeeRegistrationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistrationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
